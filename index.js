d27a44d7476fe6c96d31cb466e16f22b571a31e8
jagarcia05 <joseantoniogarciapascual17@gmail.com>
2024-11-21 08:23:33 +0100

    primercommit

--- index.js
+++ index.js
@@ -19,8 +19,34 @@ function procesardatos(data){
  
   
 data.forEach(game => {
-  console.log(game.slug)
+  console.log(game.name)
+  console.log(game.released)
+  console.log(game.background_image)
+ console.log(game)
+  game.platforms.forEach(plataform=> {
+    console.log(plataform.platform.name)
   });
+
+ 
+  game.ratings.forEach(rating=> {
+    console.log(rating.title ,rating.percent)
+    
+  }
+  );
+  let genero= []
+  game.genres.forEach(genre=> {
+    genero.push(genre.name)
+    
+    
+    
+   
+    
+  }
+  );
+  console.log(genero)
+  
+  });
+ 
  
 }
 getGame(1,40)
